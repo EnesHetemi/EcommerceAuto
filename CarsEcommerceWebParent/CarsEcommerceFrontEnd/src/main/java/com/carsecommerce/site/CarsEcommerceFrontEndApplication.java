041b702d82ecb9bf2b209dd5d7eef02472e186f6
EnesHetemi <eneshetemi3@gmail.com>
2023-04-23 10:52:32 +0200

    Inicializimi

---
+++ CarsEcommerceWebParent/CarsEcommerceFrontEnd/src/main/java/com/carsecommerce/site/CarsEcommerceFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.carsecommerce.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsEcommerceFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsEcommerceFrontEndApplication.class, args);
+	}
+
+}
