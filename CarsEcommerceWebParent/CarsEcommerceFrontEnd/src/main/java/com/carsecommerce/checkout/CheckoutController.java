8b754e53e713e07e511faddd97b4ff0379fb98f2
EnesHetemi <eneshetemi3@gmail.com>
2023-05-21 00:55:03 +0200

    Konfirmimi i porosis ne email per konsumatoret

--- CarsEcommerceWebParent/CarsEcommerceFrontEnd/src/main/java/com/carsecommerce/checkout/CheckoutController.java
+++ CarsEcommerceWebParent/CarsEcommerceFrontEnd/src/main/java/com/carsecommerce/checkout/CheckoutController.java
@@ -1,21 +1,32 @@
 package com.carsecommerce.checkout;
 
+import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.carsecommerce.customer.CustomerController;
+import com.carsecommerce.Utility;
 import com.carsecommerce.address.AddressService;
 import com.carsecommerce.common.entity.Address;
 import com.carsecommerce.common.entity.CartItem;
 import com.carsecommerce.common.entity.Customer;
 import com.carsecommerce.order.OrderService;
+import com.carsecommerce.setting.EmailSettingBag;
+import com.carsecommerce.setting.SettingService;
+import com.carsecommerce.common.entity.order.Order;
 import com.carsecommerce.common.entity.order.PaymentMethod;
 import com.carsecommerce.customer.CustomerService;
 import com.carsecommerce.shoppingcart.ShoppingCartService;
@@ -29,6 +40,7 @@ public class CheckoutController {
 	@Autowired private ShoppingCartService cartService;
 	@Autowired private CustomerController customerController;
 	@Autowired private OrderService orderService;
+	@Autowired private SettingService settingService;
 
 	@GetMapping("/checkout")
 	public String showCheckoutPage(Model model, HttpServletRequest request) {
@@ -53,7 +65,8 @@ public class CheckoutController {
 	
 	
 	@PostMapping("/place_order")
-	public String placeOrder(HttpServletRequest request) {
+	public String placeOrder(HttpServletRequest request) 
+			throws UnsupportedEncodingException, MessagingException {
 		String paymentType = request.getParameter("paymentMethod");
 		PaymentMethod paymentMethod = PaymentMethod.valueOf(paymentType);
 
@@ -64,10 +77,45 @@ public class CheckoutController {
 		List<CartItem> cartItems = cartService.listCartItems(customer);
 		CheckoutInfo checkoutInfo = checkoutService.prepareCheckout(cartItems);
 
-		orderService.createOrder(customer, defaultAddress, cartItems, paymentMethod, checkoutInfo);
+		Order createdOrder = orderService.createOrder(customer, defaultAddress, cartItems, paymentMethod, checkoutInfo);
 		cartService.deleteByCustomer(customer);
+		
+		sendOrderConfirmationEmail(request, createdOrder);
 
 		return "checkout/order_completed";
+	}
+	
+	private void sendOrderConfirmationEmail(HttpServletRequest request, Order order) 
+			throws UnsupportedEncodingException, MessagingException {
+		EmailSettingBag emailSettings = settingService.getEmailSettings();
+		JavaMailSenderImpl mailSender = Utility.prepareMailSender(emailSettings);
+		mailSender.setDefaultEncoding("utf-8");
+
+		String toAddress = order.getCustomer().getEmail();
+		String subject = emailSettings.getOrderConfirmationSubject();
+		String content = emailSettings.getOrderConfirmationContent();
+
+		subject = subject.replace("[[orderId]]", String.valueOf(order.getId()));
+
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+
+		helper.setFrom(emailSettings.getFromAddress(), emailSettings.getSenderName());
+		helper.setTo(toAddress);
+		helper.setSubject(subject);
+
+		DateFormat dateFormatter =  new SimpleDateFormat("HH:mm:ss E, dd MMM yyyy");
+		String orderTime = dateFormatter.format(order.getOrderTime());
+
+		content = content.replace("[[name]]", order.getCustomer().getFullName());
+		content = content.replace("[[orderId]]", String.valueOf(order.getId()));
+		content = content.replace("[[orderTime]]", orderTime);
+		content = content.replace("[[shippingAddress]]", order.getShippingAddress());
+		content = content.replace("[[total]]", String.valueOf(order.getTotal()));
+		content = content.replace("[[paymentMethod]]", order.getPaymentMethod().toString());
+
+		helper.setText(content, true);
+		mailSender.send(message);		
 	}
 
 	private Customer getAuthenticatedCustomer(HttpServletRequest request) {
