8d75b4c4dd878077961913cb2f932aa51e79fe39
EnesHetemi <eneshetemi3@gmail.com>
2023-05-27 12:07:39 +0200

    Raporti i shitjeve sipas datave" j

--- CarsEcommerceWebParent/CarsEcommerceBackEnd/src/main/resources/static/js/sales_report_by_date.js
+++ CarsEcommerceWebParent/CarsEcommerceBackEnd/src/main/resources/static/js/sales_report_by_date.js
@@ -2,8 +2,16 @@ var data;
 var chartOptions;
 var totalGrossSales;
 var totalOrders;
+var startDateField;
+var endDateField;
+
+var MILLISECONDS_A_DAY = 24 * 60 * 60 * 1000;
 
 $(document).ready(function() {
+	divCustomDateRange = $("#divCustomDateRange");
+	startDateField = document.getElementById('startDate');
+	endDateField = document.getElementById('endDate');
+	
 	$(".button-sales-by-date").on("click", function() {
 
 		$(".button-sales-by-date").each(function(e) {
@@ -12,13 +20,61 @@ $(document).ready(function() {
 
 		$(this).removeClass('btn-light').addClass('btn-primary');
 
-		period = $(this).attr("period");		
-		loadSalesReportByDate(period);
+		period = $(this).attr("period");
+		if (period) {
+			loadSalesReportByDate(period);
+			divCustomDateRange.addClass("d-none");
+		} else {
+			divCustomDateRange.removeClass("d-none");
+		}		
+	});
+
+	initCustomDateRange();
+
+	$("#buttonViewReportByDateRange").on("click", function(e) {
+		validateDateRange();
 	});
 });
 
+function validateDateRange() {
+	days = calculateDays();
+
+	startDateField.setCustomValidity("");
+
+	if (days >= 7 && days <= 30) {
+		loadSalesReportByDate("custom");
+	} else {
+		startDateField.setCustomValidity("Data duhet të jetë në rangun 7 deri ne 30 ditë");
+		startDateField.reportValidity();
+	}
+}
+
+function calculateDays() {
+	startDate = startDateField.valueAsDate;
+	endDate = endDateField.valueAsDate;
+
+	differenceInMilliseconds = endDate - startDate;
+	return differenceInMilliseconds / MILLISECONDS_A_DAY;
+}
+
+function initCustomDateRange() {
+	toDate = new Date();
+	endDateField.valueAsDate = toDate;
+
+	fromDate = new Date();
+	fromDate.setDate(toDate.getDate() - 30);
+	startDateField.valueAsDate = fromDate;
+}	
+
 function loadSalesReportByDate(period) {
-	requestURL = contextPath + "reports/sales_by_date/" + period;
+	if (period == "custom") {
+		startDate = $("#startDate").val();
+		endDate = $("#endDate").val();
+
+		requestURL = contextPath + "reports/sales_by_date/" + startDate + "/" + endDate;
+	} else {
+		requestURL = contextPath + "reports/sales_by_date/" + period;		
+	}
 
 	$.get(requestURL, function(responseJSON) {
 		prepareChartData(responseJSON);
@@ -85,6 +141,7 @@ function getChartTitle(period) {
 	if (period == "last_28_days") return "Shitjet ne periudhen 28 Ditore";
 	if (period == "last_6_months") return "Shitjet ne periudhen 6 Mujore";
 	if (period == "last_year") return "Shitjet ne periudhen 1 vjeqare";
+	if (period == "custom") return "Sipas Dates";
 
 	return "";
 }
@@ -94,6 +151,7 @@ function getDenominator(period) {
 	if (period == "last_28_days") return 28;
 	if (period == "last_6_months") return 6;
 	if (period == "last_year") return 12;
+	if (period == "custom") return calculateDays();
 
 	return 7;
 }
